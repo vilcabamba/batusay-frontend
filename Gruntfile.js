36641d9a68ce585155dcb0def657a7b0844098b5
Fabricio Flores <fabrifloresg@gmail.com>
2017-05-30 11:54:35 -0500

    build directive templates as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,7 +379,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: 'views/{,**/}*.html',
         dest: '.tmp/templateCache.js'
       }
     },
